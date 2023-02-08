2fa291ab2f0335bd3e2de427c3bd1be60f17ed91
davey-mwaura <davidmburu50@gmail.com>
2023-02-09 01:32:40 +0300

    Initial commit

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,28 @@
 class Dog
+    attr_accessor :name, :breed, :age
+  
+    def initialize(name, breed, age)
+      @name = name
+      @breed = breed
+      @age = age
+    end
+  
+  
+    def display_info
+      puts "Name: #{@name}"
+      puts "Breed: #{@breed}"
+      puts "Age: #{@age}"
+    end
 
-end
+    def bark
+        puts "#{@name} barks!"
+      end
+      
+  end
+
+  dog = Dog.new("Rex", "Labrador", 3)
+ 
+puts dog.name # Rex
+puts dog.breed # Labrador
+puts dog.age # 3
+dog.bark # Rex barks!
